844f44da56618d5c7b1d25d46a8797fa8e853f26
noodles4u <louisdesmett@gmail.com>
2023-08-22 15:28:41 +0200

    simplified navbar from tailwind compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,57 @@
 import { useEffect, useState } from "react";
-import beehiveLogo from './assets/img/beehive.png'
-import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from "react-router-dom";
+import beehiveLogo from "./assets/img/beehive.png";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams,
+} from "react-router-dom";
 
 import "./index.css";
 
 import NoMatch from "./components/404";
 import Login from "./components/authi";
-import Example from "./components/navbar";
+import Nav from "./components/navbar";
 
-const Home=()=> {
-	return (
-		<div style={{ padding: 20 }}>
-			<h2>Home</h2>
-      <img width={60} height= {60}src={beehiveLogo}/>
-			<p>Welcome to Bidhive</p>
-		</div>
-	);
-}
-
-
-const App = () => {
-	const [users, setUsers] = useState([]);
-
-	const fetchData = async () => {
-		await fetch("https://auction.oxomoto.co/")
-			.then((response) => {
-				console.log(response);
-				// return response.json();
-			})
-			.then((data) => {
-				setUsers(data);
-			});
-	};
-
-	useEffect(() => {
-		fetchData();
-	}, []);
-
-	return (
-		<Router>
-
-			<nav style={{ margin: 10 }}>
-
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        <Example/>
-				<Link to="/" style={{ padding: 5 }}>
-					Home
-				</Link>
-				<Link to="/login" style={{ padding: 5 }}>
-					Login
-				</Link>
-				
-			</nav>
-
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="*" element={<NoMatch />} />
-			</Routes>
-		</Router>
-	);
+const Home = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home</h2>
+      <img width={60} height={60} src={beehiveLogo} />
+      <p>Welcome to Bidhive</p>
+    </div>
+  );
 };
 
-export default App
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://auction.oxomoto.co/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Router>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
