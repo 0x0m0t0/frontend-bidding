a767e0c3c28e9158349f435bd198a101c1c5020e
noodles4u <louisdesmett@gmail.com>
2023-08-22 12:43:57 +0200

    iclude jsx vite test

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,41 @@ import react from '@vitejs/plugin-react'
 import path from 'path' 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
+  esbuild: 
+  
+  {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
+  
+      loader: "jsx", // OR "jsx"
+      include: [
+        // Add this for business-as-usual behaviour for .jsx and .tsx files
+        "src/**/*.jsx",
+        "src/**/*.tsx",
+        "node_modules/**/*.jsx",
+        "node_modules/**/*.tsx",
+  
+        // // Add the specific files you want to allow JSX syntax in
+        // "src/LocalJsxInJsComponent.js",
+        // "node_modules/bad-jsx-in-js-component/index.js",
+        // "node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js",
+        // "node_modules/bad-jsx-in-js-component/ts/index.ts",
+        // "node_modules/bad-jsx-in-js-component/ts/BadTSXinTS.ts",
+  
+        // // --- OR ---
+  
+        // // Add these lines to allow all .js files to contain JSX
+        // "src/**/*.js",
+        // "node_modules/**/*.js",
+  
+        // // Add these lines to allow all .ts files to contain JSX
+        // "src/**/*.ts",
+        // "node_modules/**/*.ts",
+      ],
+    
   },
+
+  
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
@@ -16,6 +47,8 @@ export default defineConfig({
   },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', ...]
   },
  
   base: './',
