f6bb3a626878d3cbf476227533be635dce012d87
noodles4u <louisdesmett@gmail.com>
2023-08-25 17:29:34 +0200

    small cookie protection

--- src/components/newitem.jsx
+++ src/components/newitem.jsx
@@ -1,7 +1,28 @@
 import { useState, useEffect } from "react";
+import { CookiesProvider, useCookies } from "react-cookie";
+
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
+const No = () => {
+  if (cookies.user) {
+    fetch(`${endpoint}/historic/1`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        console.log(post);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+};
+
 const NewItem = () => {
+  const [cookies, setCookie] = useCookies(["user"]);
   const [item, setItem] = useState({
     user_id: "129",
     itemName: "",
@@ -20,13 +41,18 @@ const NewItem = () => {
     }));
   };
 
-  const No = () => {
-    useEffect(() => {
-      fetch(`${endpoint}/historic/1`, {
-        method: "GET",
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    console.log(item);
+
+    if (cookies.user) {
+      fetch(`${endpoint}/my_auction`, {
+        method: "POST",
+        body: JSON.stringify(item),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
+          authentication: cookies.user,
         },
       })
         .then((res) => res.json())
@@ -36,33 +62,13 @@ const NewItem = () => {
         .catch((err) => {
           console.log(err.message);
         });
-    }, []);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(item);
-
-    fetch(`${endpoint}/my_auction`, {
-      method: "POST",
-      body: JSON.stringify(item),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        console.log(post);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    } else {
+      alert("Not allowed, please login");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <No />
+    <form onSubmit={({ handleSubmit }, { No })}>
       <label>
         Item name:
         <input
