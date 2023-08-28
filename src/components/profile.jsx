917a6b69a5ef2cc1b67019205a52ecb5e4b96a43
noodles4u <louisdesmett@gmail.com>
2023-08-28 15:27:17 +0200

    user in utils fetch

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,30 +1,34 @@
 import { useState } from "react";
+import user from "../utils/user_account";
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
 const Profile = ({ users }) => {
   let userId = 129;
-  const Biddings = () => {
-    fetch(`${endpoint}/my_bidding/${userId}`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        console.log(post);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+  const [userInfo, setUserInfo] = useState([]);
+  user({ endpoint, userId, setUserInfo });
 
-  Biddings();
+  // const Biddings = () => {
+  //   fetch(`${endpoint}/my_bidding/${userId}`, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((post) => {
+  //       console.log(post);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // };
+
+  // Biddings();
 
   return (
     <div className="flex flex-col self-center items-center">
       <h2 className="h-60 w-60 m-20 text-2xl">
-        Hello, welcome to your profile {users.name}
+        Hello, welcome to your profile <em>{userInfo[0]?.name}</em>
       </h2>
     </div>
   );
