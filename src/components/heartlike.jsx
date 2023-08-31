8f2b8d8c82049784c3de04bdb63f2bd8810fd937
noodles4u <louisdesmett@gmail.com>
2023-08-31 12:02:04 +0200

    async like checker

--- src/components/heartlike.jsx
+++ src/components/heartlike.jsx
@@ -3,13 +3,54 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
+/// to change, use props
+let lobbyId = 1;
+
 export const HeartLike = () => {
+  const [likedLobbies, setLikedLobbies] = useState([]);
   const [likes, setLikes] = useState();
   const [isClicked, setIsClicked] = useState(false);
   const [cookies] = useCookies(["user"], ["user_id"]);
 
+  const likeChecker = () => {
+    console.log("its called");
+    if (likedLobbies.length > 0) {
+      const isLiked = likedLobbies.findIndex(
+        (item) => item.id_lobby === lobbyId
+      );
+      if (isLiked === 0) {
+        console.log("i did it");
+        setIsClicked(true);
+      }
+    } else {
+      console.log("nooo");
+    }
+  };
+
+  const ifLiked = () => {
+    fetch(`${endpoint}/account/like/${cookies.user_id}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "https://platform.oxomoto.co/",
+        authentication: cookies.user,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) throw new err(res.status);
+        else return res.json();
+      })
+      .then((data) => {
+        setLikedLobbies(data);
+        console.log(data[0]);
+      })
+      // .then(() => likeChecker())
+      .catch((err) => {
+        return err;
+      });
+  };
   const amountLikes = () => {
-    fetch(`${endpoint}/lobby/update_lobby/1`, {
+    fetch(`${endpoint}/lobby/update_lobby/${lobbyId}`, {
       method: "GET",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
@@ -28,7 +69,6 @@ export const HeartLike = () => {
         return err;
       });
   };
-  console.log(likes);
 
   const updateLike = () => {
     if (cookies.user) {
@@ -57,9 +97,11 @@ export const HeartLike = () => {
     updateLike();
     setIsClicked(!isClicked);
   };
+
   useEffect(() => {
+    ifLiked();
     amountLikes();
-  }, [handleClick]);
+  }, []);
 
   if (isClicked) {
     return (
