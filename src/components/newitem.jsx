f162d0e0b19a7bfdca89489b3c4d38bb68bc02b7
noodles4u <louisdesmett@gmail.com>
2023-09-11 16:43:16 +0200

    redirect newitem

--- src/components/newitem.jsx
+++ src/components/newitem.jsx
@@ -4,9 +4,12 @@ import DateTimePicker from "react-datetime-picker";
 import "react-datetime-picker/dist/DateTimePicker.css";
 import "react-calendar/dist/Calendar.css";
 import "react-clock/dist/Clock.css";
+import { useNavigate } from "react-router-dom";
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 
 const NewItem = () => {
+  const [isSent, setisSent] = useState(true);
+  const navigate = useNavigate();
   const [date, setDate] = useState(new Date());
   const [cookies, setCookie] = useCookies(["user"]);
   const [item, setItem] = useState({
@@ -53,8 +56,14 @@ const NewItem = () => {
       });
   };
 
+  const Redirect = () => {
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    isSent ? setisSent(false) : setisSent(true);
 
     console.log(item);
 
@@ -82,97 +91,104 @@ const NewItem = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <No />
-        <label>
-          Item name:
-          <input
-            type="text"
-            name="name"
-            required
-            value={item.itemName}
-            onChange={(e) => {
-              e.preventDefault();
-              updateForm("itemName", e);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Description:
-          <input
-            type="text"
-            value={item.itemDescription}
-            onChange={(e) => {
-              e.preventDefault();
-              updateForm("itemDescription", e);
-            }}
-          />
-        </label>
+      {isSent ? (
+        <form onSubmit={handleSubmit}>
+          <No />
+          <label>
+            Item name:
+            <input
+              type="text"
+              name="name"
+              required
+              value={item.itemName}
+              onChange={(e) => {
+                e.preventDefault();
+                updateForm("itemName", e);
+              }}
+            />
+          </label>
+          <br />
+          <label>
+            Description:
+            <input
+              type="text"
+              value={item.itemDescription}
+              onChange={(e) => {
+                e.preventDefault();
+                updateForm("itemDescription", e);
+              }}
+            />
+          </label>
 
-        <br />
+          <br />
 
-        <label>
-          Image cover:
-          <input
-            type="url"
-            name="post"
-            required
-            value={item.coverLobby}
-            onChange={(e) => {
-              e.preventDefault();
-              updateForm("coverLobby", e);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Start Time:
-          <DateTimePicker
-            amPmAriaLabel="Select AM/PM"
-            calendarAriaLabel="Toggle calendar"
-            clearAriaLabel="Clear value"
-            dayAriaLabel="Day"
-            hourAriaLabel="Hour"
-            maxDetail="second"
-            minuteAriaLabel="Minute"
-            monthAriaLabel="Month"
-            nativeInputAriaLabel="Date and time"
-            onChange={handleDateChange}
-            secondAriaLabel="Second"
-            value={date}
-            yearAriaLabel="Year"
-          />
-        </label>
-        <br />
-        <label>
-          Tags:
-          <input
-            type="text"
-            name="tags"
-            value={item.tags}
-            onChange={(e) => {
-              e.preventDefault();
-              updateForm("tags", e);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Pictures:
-          <input
-            type="url"
-            name="pictures"
-            value={item.pictures}
-            onChange={(e) => {
-              e.preventDefault();
-              updateForm("pictures", e);
-            }}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+          <label>
+            Image cover:
+            <input
+              type="url"
+              name="post"
+              required
+              value={item.coverLobby}
+              onChange={(e) => {
+                e.preventDefault();
+                updateForm("coverLobby", e);
+              }}
+            />
+          </label>
+          <br />
+          <label>
+            Start Time:
+            <DateTimePicker
+              amPmAriaLabel="Select AM/PM"
+              calendarAriaLabel="Toggle calendar"
+              clearAriaLabel="Clear value"
+              dayAriaLabel="Day"
+              hourAriaLabel="Hour"
+              maxDetail="second"
+              minuteAriaLabel="Minute"
+              monthAriaLabel="Month"
+              nativeInputAriaLabel="Date and time"
+              onChange={handleDateChange}
+              secondAriaLabel="Second"
+              value={date}
+              yearAriaLabel="Year"
+            />
+          </label>
+          <br />
+          <label>
+            Tags:
+            <input
+              type="text"
+              name="tags"
+              value={item.tags}
+              onChange={(e) => {
+                e.preventDefault();
+                updateForm("tags", e);
+              }}
+            />
+          </label>
+          <br />
+          <label>
+            Pictures:
+            <input
+              type="url"
+              name="pictures"
+              value={item.pictures}
+              onChange={(e) => {
+                e.preventDefault();
+                updateForm("pictures", e);
+              }}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <>
+          <p>Thank you, you'll be redirected in 3 seconds to the homepage</p>
+          <Redirect />
+        </>
+      )}
     </>
   );
 };
