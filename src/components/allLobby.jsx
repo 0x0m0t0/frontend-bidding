36459be9a82cdd34340a3905eb17a4d9f901f057
Safa <safae.khess@gmail.com>
2023-09-13 00:08:01 +0200

    j'essaye dif dispositions

--- src/components/allLobby.jsx
+++ src/components/allLobby.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 import { Cookies, useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
+import './allLobby.css'
 
 const AllLobby = () => {
   const [cookies] = useCookies(["user"], ["user_id"]);
@@ -36,37 +37,31 @@ const AllLobby = () => {
 
   return (
     <>
-      <h1>Here are all the lobbies</h1>
+      <h1 className="text-4xl mb-10"> All the lobbies</h1>
 
-      <section className="flex flex-wrap">
-        {lobbies?.length > 0 ? (
-          lobbies?.map((item, i) => {
-            return (
-              <div
-                key={item?.id + item?.created_at}
-                className="p-2 max-w-[10rem]"
-              >
-                <div className="border p-2">
-                  <p>{item?.name}</p>
-                  <div className="max-w-[10rem] object-cover">
-                    <img
-                      className="rounded-2xl"
-                      src={item?.cover_lobby}
-                      alt={item?.name}
-                    />
-                  </div>
-                  <p>likes: {item?.likes}</p>
-                  <Link to={`/lobby/${item?.id}`} className="underline">
-                    Lobby {item?.id}
-                  </Link>
-                </div>
-              </div>
-            );
-          })
-        ) : (
-          <p>nothing yet</p>
-        )}
-      </section>
+      <section className="flex flex-wrap -mx-4">
+  {lobbies?.length > 0 ? (
+    lobbies?.map((item, i) => {
+      return (
+        <div key={item?.id + item?.created_at} className="p-3 max-w-[calc(25% - 50px)] mx-4 mb-4">
+          <div className="border p-2">
+            <p>{item?.name}</p>
+            <div className="max-w-[10rem] object-cover">
+              <img className="rounded-2xl" src={item?.cover_lobby} alt={item?.name} />
+            </div>
+            <p>likes: {item?.likes}</p>
+            <Link to={`/lobby/${item?.id}`} className="underline">
+              Lobby {item?.id}
+            </Link>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <p>nothing yet</p>
+  )}
+</section>
+
     </>
   );
 };
