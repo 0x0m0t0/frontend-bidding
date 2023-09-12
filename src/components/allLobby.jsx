27f780f56db6b48cb2a793e505181df07558cfc4
Safa <safae.khess@gmail.com>
2023-09-13 01:39:44 +0200

    a quelques details pres all loby fini

--- src/components/allLobby.jsx
+++ src/components/allLobby.jsx
@@ -3,6 +3,7 @@ const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 import { Cookies, useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import './allLobby.css'
+import { HeartLike } from "./heartlike.jsx";
 
 const AllLobby = () => {
   const [cookies] = useCookies(["user"], ["user_id"]);
@@ -47,9 +48,9 @@ const AllLobby = () => {
           <div className="border collection2 p-2">
             <p className="item">{item?.name}</p>
             <div className="max-w-[10rem] object-cover">
-              <img className="rounded " src={item?.cover_lobby} alt={item?.name} />
+              <img className="rounded " src={item?.cover_lobby} alt={item?.name} style={{ width: '243px' }}/>
             </div>
-            <p>likes: {item?.likes}</p>
+            <p className="heart"> ♥ {item?.likes}</p>
             <Link to={`/lobby/${item?.id}`} className="underline">
               Lobby {item?.id}
             </Link>
