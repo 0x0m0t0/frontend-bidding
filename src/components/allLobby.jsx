5f6cec4d9dce676ee5954b0b11f9d7e6d60e20b0
Safa <safae.khess@gmail.com>
2023-09-15 13:07:05 +0200

    ...

--- src/components/allLobby.jsx
+++ src/components/allLobby.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 const endpoint = import.meta.env.VITE_REACT_APP_ENDPOINT;
 import { Cookies, useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
-import "./home.css";
+import "./allLobby.css";
 import { HeartLike } from "./heartlike.jsx";
 
 
